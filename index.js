2dec4ae6cf8b64ce9b9262cf399659352c6f3d22
Joshua Jones <joshuajones2010@gmail.com>
2023-12-14 11:48:37 +1100

    stringify object

--- index.js
+++ index.js
@@ -511,7 +511,7 @@ Lock.prototype.release = function(callback)
         {
             if (error && error.code === "ConditionalCheckFailedException")
             {
-                const err = new Error(`Failed to release lock ${buildExpressionAttributeNames(self)} with recordVersionNumber ${self._recordVersionNumber}.`);
+                const err = new Error(`Failed to release lock ${JSON.stringify(buildExpressionAttributeNames(self))} with recordVersionNumber ${self._recordVersionNumber}.`);
                 err.code = "FailedToReleaseLock";
                 err.originalError = error;
                 return callback(err);
